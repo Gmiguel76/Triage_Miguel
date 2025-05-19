5f1cc3d0d241b9a5ebd7ec3a6535bb06c26a408d
Gloria Miguel <gloria.miguel@hotmail.com>
2025-05-19 06:54:20 -0300

    Primer cambio

---
+++ script.js
@@ -0,0 +1,48 @@
+let pacientes = [];
+
+function agregarPrioridad(sintoma){
+    if (sintoma === "dificultad respiratoria grave" || sintoma ==="convulsiones" || sintoma === "traumatismo grave"){
+        alert("🔴Codigo Rojo: Atencion inmediata");
+        return "Codigo rojo";
+    } else if (sintoma === "broncoespasmo" || sintoma === "fiebre alta"){
+        alert("🟡Codigo Amarillo: Atencion prioritaria");
+        return "Codigo amarillo";
+    } else {
+        alert("🟢Codigo Verde: Atencion normal");
+        return "Codigo verde";
+    }
+}
+
+function ingresarPaciente(){
+    let nombre = prompt("Ingresar nombre y apellido");
+    let edad = prompt("Ingresar edad");
+    let sintoma = prompt("Ingresar sintoma");
+    let prioridad = agregarPrioridad(sintoma);
+return {nombre, edad, sintoma, prioridad};
+}
+
+
+function agregarPacienteTabla() {
+    let nuevoPacienteIngresado = ingresarPaciente();
+    pacientes.push(nuevoPacienteIngresado);
+
+    
+const tablaPacientes = document.getElementById("tablaPacientes")
+
+
+let fila = tablaPacientes.insertRow();
+
+    
+let celdaNombre = fila.insertCell(0);
+let celdaEdad = fila.insertCell(1);
+let celdaSintoma = fila.insertCell(2);
+let celdaPrioridad = fila.insertCell(3);
+
+celdaNombre.textContent = nuevoPacienteIngresado.nombre;
+celdaEdad.textContent = nuevoPacienteIngresado.edad;
+celdaSintoma.textContent = nuevoPacienteIngresado.sintoma;
+celdaPrioridad.textContent = nuevoPacienteIngresado.prioridad;
+}
+
+document.getElementById("botonIngreso").addEventListener("click", agregarPacienteTabla);
+
